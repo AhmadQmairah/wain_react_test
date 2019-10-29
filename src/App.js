dec11206bb40345b16b52ed1812b81c7f3a1ffb7
Ahmad Qmairah <AhmadQmairah@gmail.com>
2019-10-29 08:50:43 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import socketIOClient from "socket.io-client";
 
 function App() {
+  //Initialize the connection to the server
+  let socket1 = socketIOClient("192.168.100.232");
+  let socket2 = socketIOClient("192.168.100.232");
+  //Listen to event hi from the server
+  socket1.on("hi", function(data) {
+    console.log("suck my dick");
+  });
+  //Send to the server with event "restaurant room"
+
+  function join() {
+    socket1.emit("join", {
+      id: "2"
+    });
+    socket2.emit("join", {
+      id: "2"
+    });
+  }
+  function sendShit() {
+    socket2.emit("hi", {
+      id: "2"
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => join()}>Join room</button>
+      <button onClick={() => sendShit()}>Send shit</button>
     </div>
   );
 }
